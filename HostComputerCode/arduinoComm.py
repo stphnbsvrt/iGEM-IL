02e3b2d5385c55515054b9ef1970d0b0be0a9712
Adam Cohen <adamcohen578@gmail.com>
2013-07-25 15:47:52 -0500

    Update to sample host computer code for python.

--- HostComputerCode/arduinoComm.py
+++ HostComputerCode/arduinoComm.py
@@ -1,21 +1,42 @@
 import serial
+import time
+import subprocess
+
 
 ard = serial.Serial()
+messages = []
+modules = []
 
 def setup():
     try:
         global ard
-        ard = serial.Serial('/dev/ttyACM0', 9600)
+        ard = serial.Serial('/dev/ttyACM0', 9600, timeout=.01)
         print("CONNECTION SUCCESFUL")
     except:
         print("ERROR: device connection failed")
 
+def getControlModules():
+    modStrings = ["practice"]
+    global modules
+    for mod in modStrings:
+        try:
+            exec("import " + mod)
+            modules.append(eval(mod))
+        except Error:
+            print "Module " + mod + " does not exist."
+
+
 def writeln(text):
+    global messages
+    messages += [text, time.time()]
     text += '\n'
     ard.write(text)
 
 def write(text):
     ard.write(text)
+
+def read(i):
+    print(ard.read(i))
 
 def readln():
     print(ard.readline())
